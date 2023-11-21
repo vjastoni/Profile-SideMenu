a827ad4765c04ed6186e5640ee9fdfa037d8cf2a
vjastoni <vjastoni13@gmail.com>
2023-11-21 12:48:15 +0800

    fixes expand and minimize faqs icon

--- src/components/FaqsComponent.jsx
+++ src/components/FaqsComponent.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 import styles from './FaqsComponent.module.css';
 
+import { useState } from 'react';
+
 import ExpandIcon from '../assets/ExpandIcon.png';
+import MinimizeIcon from '../assets/MinimizeIcon.png';
 
 const FaqsComponent = ({ question, answer }) => {
+  const [showIcon, setShowIcon] = useState(false);
   return (
     <details className={styles.container}>
-      <summary>
+      <summary
+        onClick={() => {
+          setShowIcon(!showIcon);
+        }}
+      >
         {question}
-        <img src={ExpandIcon} />
+        <img src={`${showIcon ? MinimizeIcon : ExpandIcon}`} />
       </summary>
       <p>{answer}</p>
     </details>
