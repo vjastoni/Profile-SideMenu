c5c7cd20e916618de9db7115d55426a29c0b8bc6
vjastoni <vjastoni13@gmail.com>
2023-11-21 22:09:51 +0800

    fix ui bugs

--- src/components/TechnicalSupport.jsx
+++ src/components/TechnicalSupport.jsx
@@ -4,58 +4,47 @@ const TechnicalSupport = () => {
   return (
     <div className={styles.technicalContainer}>
       <h1>Technical Support</h1>
-      <div className={styles.form}>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-        >
-          <div className={styles.colWrapper}>
-            <div className={styles.firstCol}>
-              <div>
-                <p>Full name</p>
-                <input
-                  className={styles.shorter}
-                  type="name"
-                  placeholder="Ex. Youssef Bsheer"
-                />
-              </div>
-              <div>
-                <p>Email</p>
-                <input
-                  className={styles.email}
-                  type="name"
-                  placeholder="Ex. youssefbsheer@gmail.com"
-                />
-              </div>
-            </div>
-            <div className={styles.secCol}>
-              <div>
-                <p>Phone number</p>
-                <input
-                  className={styles.shorter}
-                  type="number"
-                  placeholder="Ex. +201094828532"
-                />
-              </div>
-              <div>
-                <p>Category</p>
-                <select className={styles.select} name="select" id="select">
-                  <option value="Select Category">Select Category</option>
-                </select>
-              </div>
-            </div>
-          </div>
+      <div className={styles.colWrapper}>
+        <div className={styles.firstCol}>
           <div>
-            <p className={styles.feedback}>Feedback</p>
-            <textarea
-              className={styles.textarea}
-              placeholder="Enter your feedback"
+            <p>Full name</p>
+            <input
+              className={styles.shorter}
+              type="name"
+              placeholder="Ex. Youssef Bsheer"
             />
           </div>
-          <button className={styles.submitButton}>SUBMIT</button>
-        </form>
+          <div>
+            <p>Email</p>
+            <input
+              className={styles.email}
+              type="name"
+              placeholder="Ex. youssefbsheer@gmail.com"
+            />
+          </div>
+        </div>
+        <div className={styles.secCol}>
+          <div>
+            <p>Phone number</p>
+            <input
+              className={styles.shorter}
+              type="number"
+              placeholder="Ex. +201094828532"
+            />
+          </div>
+          <div>
+            <p>Category</p>
+            <select className={styles.select} name="select" id="select">
+              <option value="Select Category">Select Category</option>
+            </select>
+          </div>
+        </div>
       </div>
+      <div className={styles.textareaWrapper}>
+        <p className={styles.feedback}>Feedback</p>
+        <textarea className={styles.textarea} placeholder="Enter your feedback" />
+      </div>
+      <button className={styles.submitButton}>SUBMIT</button>
     </div>
   );
 };
--- src/modal/RefundSupport.jsx
+++ src/modal/RefundSupport.jsx
@@ -9,88 +9,79 @@ const RefundSupport = () => {
   return (
     <div className={styles.refundContainer}>
       <h1>Refund Support</h1>
-      <div className={styles.form}>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-        >
-          <div className={styles.colWrapper}>
-            <div className={styles.firstCol}>
-              <div>
-                <p>Name</p>
-                <input
-                  className={styles.shorter}
-                  type="name"
-                  placeholder="Ex. Youssef Bsheer"
-                />
-              </div>
-              <div>
-                <p>Email</p>
-                <input
-                  className={styles.email}
-                  type="name"
-                  placeholder="Ex. youssefbsheer@gmail.com"
-                />
-              </div>
-            </div>
-            <div className={styles.secCol}>
-              <div>
-                <p>Phone number</p>
-                <input
-                  className={styles.shorter}
-                  type="number"
-                  placeholder="Ex. +201094828532"
-                />
-              </div>
-              <div>
-                <p>Choose Transaction</p>
-                <select className={styles.select} name="select" id="select">
-                  <option value="Select Category">Choose Transaction</option>
-                </select>
-              </div>
-            </div>
+      <div className={styles.colWrapper}>
+        <div className={styles.firstCol}>
+          <div>
+            <p>Name</p>
+            <input
+              className={styles.shorter}
+              type="name"
+              placeholder="Ex. Youssef Bsheer"
+            />
           </div>
           <div>
-            <p className={styles.feedback}>Comments</p>
-            <textarea
-              className={styles.textarea}
-              placeholder="Enter your comments"
+            <p>Email</p>
+            <input
+              className={styles.email}
+              type="name"
+              placeholder="Ex. youssefbsheer@gmail.com"
             />
-
-            <div className={styles.addWrapper}>
-              <div>
-                <p>Select Image</p>
-                <input
-                  className={styles.fileType}
-                  type="file"
-                  id="myfile"
-                  name="myfile"
-                />
-              </div>
-              <div className={styles.sendTicket}>
-                <input type="checkbox" id="option" name="option" />
-                <label htmlFor="option"> Send a ticket copy via SMS </label>
-              </div>
-            </div>
           </div>
-          <button
-            onClick={() => {
-              setAllTransaction(true);
-            }}
-            className={styles.submitButton}
-          >
-            SUBMIT
-          </button>
-          {allTransaction && (
-            <AllTransactions
-              onClick={() => {
-                setAllTransaction(false);
-              }}
+        </div>
+        <div className={styles.secCol}>
+          <div>
+            <p>Phone number</p>
+            <input
+              className={styles.shorter}
+              type="number"
+              placeholder="Ex. +201094828532"
             />
-          )}
-        </form>
+          </div>
+          <div>
+            <p>Choose Transaction</p>
+            <select className={styles.select} name="select" id="select">
+              <option value="Select Category">Choose Transaction</option>
+            </select>
+          </div>
+        </div>
       </div>
+      <div className={styles.extensionDiv}>
+        <div>
+          <p className={styles.feedback}>Comments</p>
+          <textarea className={styles.textarea} placeholder="Enter your comments" />
+        </div>
+        <div className={styles.addWrapper}>
+          <div>
+            <p>Select Image</p>
+            <input
+              className={styles.fileType}
+              type="file"
+              id="myfile"
+              name="myfile"
+            />
+          </div>
+          <div className={styles.sendTicket}>
+            <input type="checkbox" id="option" name="option" />
+            <label htmlFor="option"> Send a ticket copy via SMS </label>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            setAllTransaction(true);
+          }}
+          className={styles.submitButton}
+        >
+          SUBMIT
+        </button>
+      </div>
+
+      {allTransaction && (
+        <AllTransactions
+          onClick={() => {
+            setAllTransaction(false);
+          }}
+        />
+      )}
     </div>
   );
 };
